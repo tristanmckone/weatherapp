19ffe232bd209a108a84a63137074f2686c88890
Tristan Mckone <tristanmckone@gmail.com>
2022-05-06 20:47:45 -0400

    api

--- index.js
+++ index.js
@@ -2,19 +2,33 @@
 
 
 
-//console.log(url);
-
+let user = "";
               
 const fetchUsers = () => {
-  axios.get('https://api.openweathermap.org/data/2.5/weather?lat=43.651070&lon=-79.347015&appid=352221c9d016002fdf36073f8d4dda1d')
+  axios.get(api)
       .then(response => {
-          const users = response;
+          const users = response.data;
+
           console.log(`GET list users`, users);
+
+
+          
+          
+         
       })
       .catch(error => console.error(error));
 };
 
 fetchUsers();
+
+
+
+
+
+
+
+
+
 
 
 
